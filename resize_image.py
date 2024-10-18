caf845e4301fca72337fd4ab1f4dd89322887281
Nichaojun <120893126+Nichaojun@users.noreply.github.com>
2024-10-18 21:09:11 +0800

    up

--- resize_image.py
+++ resize_image.py
@@ -1,14 +1,14 @@
 from PIL import Image
 
 # 加载本地图片
-image_path = "images/1.1.jpg"
+image_path = "images/drivedream4d.png"
 img = Image.open(image_path)
 
 # 调整图片大小为320x320
 img_resized = img.resize((320, 320))
 
 # 保存调整大小后的图片
-resized_image_path = "images/1.2.jpg"
+resized_image_path = "images/drivedream4d.png"
 img_resized.save(resized_image_path)
 
 print(f"调整后的图片已保存到 {resized_image_path}")
