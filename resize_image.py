378985d29a215e235096b56113137e646b412e35
Nichaojun <120893126+Nichaojun@users.noreply.github.com>
2024-11-30 22:55:35 +0800

    up

--- resize_image.py
+++ resize_image.py
@@ -1,14 +1,14 @@
 from PIL import Image
 
 # 加载本地图片
-image_path = "images/drivedream4d.png"
+image_path = "{1363A993-0302-4BCE-8230-A72E26427E95}.png"
 img = Image.open(image_path)
 
 # 调整图片大小为320x320
 img_resized = img.resize((320, 320))
 
 # 保存调整大小后的图片
-resized_image_path = "images/drivedream4d.png"
+resized_image_path = "images/recondreamer.png"
 img_resized.save(resized_image_path)
 
 print(f"调整后的图片已保存到 {resized_image_path}")
--- resize_video.py
+++ resize_video.py
@@ -1,10 +1,10 @@
 from moviepy.editor import VideoFileClip
 
 # 读取原始视频
-video = VideoFileClip("images/robort.mp4")
+video = VideoFileClip("images/recondreamer.mp4")
 
 # 调整视频大小为 320x320
 resized_video = video.resize(newsize=(320, 320))
 
 # 保存调整后的视频
-resized_video.write_videofile("output_video.mp4", codec="libx264")
+resized_video.write_videofile("recondreamer.mp4", codec="libx264")
